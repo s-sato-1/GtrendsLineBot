50ef247e8408b5e6883a967d203cf69172846442
s-sato-1 <53969010+s-sato-1@users.noreply.github.com>
2021-02-03 12:07:54 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,14 @@
 import configparser
 
 
-conf = configparser.ConfigParser()
-conf.read('settings.ini')
+try:
+    my_channel_access_token = os.environ.get("my_channel_access_token")
+    my_channel_secret = os.environ.get("my_channel_secret")
+except KeyError as e:
+    conf = configparser.ConfigParser()
+    conf.read('settings.ini')
 
-my_channel_access_token = conf['line']['my_channel_access_token']
-my_channel_secret = conf['line']['my_channel_secret']
+    my_channel_access_token = conf['line']['my_channel_access_token']
+    my_channel_secret = conf['line']['my_channel_secret']
+except Exception as e:
+    print(e)
